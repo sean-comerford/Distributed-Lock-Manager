6c1245fe5f19e1f2a210e8b9d19aecfb4a7cc385
sean.comerford <s2706676@ed.ac.uk>
2024-11-09 12:11:36 +0000

    Created process_request function in server.py

--- server.py
+++ server.py
@@ -36,15 +36,13 @@ class LockService(lock_pb2_grpc.LockServiceServicer):
         return self.response_cache[request_id]
 
     def initialise_cache(self, request_id):
-        '''Initialise cache with request_id and return 0'''
-        self.response_cache[request_id] = 0
+        '''Initialise cache with key = request_id and value =  0'''
+        # This value will be updated when the server finishes processing the request
+        self.response_cache[request_id] = None
 
 
     def update_cache(self, request_id, response):
         '''Store response in cache keyed by request_id'''
-            # Initialise the cache dict with key = request_id and value = 0
-            # This value will be updated when the server finishes processing the request
-            # Initalising request with empty value allows the server to recognise a repeated duplicate request
         self.response_cache[request_id] = response
 
 
@@ -54,74 +52,78 @@ class LockService(lock_pb2_grpc.LockServiceServicer):
         print(f"Received initial metadata from client: {metadata_str}")
 
 
-    def client_init(self, request, context):
-        print(f"Performing client initialization")
+    def process_request(self, context):
+        '''Process request from client to see if it has been cached etc.'''
+        # Print metadata
         self.print_metadata(context)
         # Get request ID
         request_id = self.get_request_id(context)
         # Check if request_id is cached
         if request_id in self.response_cache:
             cached_response = self.return_cached_response(request_id, context)
-            print(f"Cached response found for request {request_id}. It is {cached_response}")
-            if cached_response == 0:
+            # If the request_id is cached but its value hasnt been updated yet (i.e value is None), server is still working on request
+            if cached_response == None:
                 response = lock_pb2.Response(status=lock_pb2.Status.WORKING_ON_IT)
-                print(f"Returning response {response}")
+                print(f"Server is slow and still working on request {request_id}.")
                 return response
-            print(f"Returning cached response {cached_response}")
+            # If the request_id is cached and its value has been updated (i.e. value is not None), return the response
+            print(f"Cached response found for request {request_id}. Returning response")
             return cached_response
-        # If request_id is not cached, cache the response_ID, process it, cache the response and then return the response
+        # If request_id is not cached, initialise the cache with the request_id and set the response to None, then proceed to process the request
         print(f"No cache response found for request {request_id}. Processing request")
         self.initialise_cache(request_id)
-        print(f"Initialised cache with request {request_id} and value {self.response_cache[request_id]}")
+        return None
+
+
+    def client_init(self, request, context):
+        '''Initialise client'''
+        # Process request
+        response = self.process_request(context)
+        if response:
+            return response
+        # If there is no response ready, process the request and create a response
+        request_id = self.get_request_id(context)
         with self.lock:
-            # simulate slow server
-            print(f"Simulating slow server: sleeping for 5 seconds")
-            time.sleep(7)
+            # Simulate slow server
+            print(f"Simulating slow server")
+            time.sleep(3)
             print(f"Server finished sleeping")
+            # Process request
             self.client_counter += 1
             client_id = self.client_counter
         response = lock_pb2.Response(status= lock_pb2.Status.SUCCESS, id_num=client_id)
-        print(f"Response is {response}")
         self.update_cache(request_id, response)
-        print(f"The updated cache is {self.response_cache}")
-        # Testing packet loss
-        if random.random() < 0.9:
-            print("Simulating packet loss: dropping response from server")
-            # Set long time to simulate packet loss
-            time.sleep(15)
-        print(f"Server has finished processing request {request_id}. Client ID is {client_id}")
-        print(f"Returning response {response}")
+        print(f"Server has finished processing request {request_id}. Client ID is {client_id}, response is {response}")
         return response
     
     def client_close(self, request, context):
-        self.print_metadata(context)
-        # Get request ID and check if request is cached
+        '''Close client'''
+        # Process request
+        response = self.process_request(context)
+        if response:
+            return response
+        # If there is no response ready, process the request and create a response
         request_id = self.get_request_id(context)
-        cached_response = self.return_cached_response(request_id, context)
-        if cached_response:
-            return cached_response
-        # If request is not cached, process request and cache response
         with self.lock:
+            # If the client closing is the lock owner, release the lock
             if self.lock_owner == request.rc:
                 self.locked = False
                 self.condition.notify_all()
                 self.lock_owner = None
                 print(f"Lock released due to client {request.rc} closing.")
-        
+            # If the client closing is not the lock owner, just close the client
             response = lock_pb2.Response(status=lock_pb2.Status.SUCCESS)
-            self.cache_response(request_id, response)
+            self.update_cache(request_id, response)
             print(f"Client {request.rc} closed") #How else to close?
             return response
     
     def lock_acquire(self, request, context):
-        print(f"Performing lock acquisition")
-        self.print_metadata(context)
-        # Get request ID and check if request is cached
+        # Process request
+        response = self.process_request(context)
+        if response:
+            return response
+        # If there is no response ready, process the request and create a response
         request_id = self.get_request_id(context)
-        cached_response = self.return_cached_response(request_id, context)
-        if cached_response:
-            return cached_response
-        # If request is not cached, process request and cache response
         with self.condition:
             while self.locked:
                 print(f"Client {request.client_id} waiting for lock.")
@@ -129,81 +131,65 @@ class LockService(lock_pb2_grpc.LockServiceServicer):
             self.locked = True
             self.lock_owner = request.client_id
             response = lock_pb2.Response(status=lock_pb2.Status.SUCCESS)
-            self.cache_response(request_id, response)
+            self.update_cache(request_id, response)
             print(f"Lock granted to client {request.client_id}")
             return response
         
     def lock_release(self, request, context):
-        self.print_metadata(context)
-        # Get request ID and check if request is cached
+        # Process request
+        response = self.process_request(context)
+        if response:
+            return response
+        # If there is no response ready, process the request and create a response
         request_id = self.get_request_id(context)
-        cached_response = self.return_cached_response(request_id, context)
-        if cached_response:
-            return cached_response
-        # If request is not cached, process request and cache response
         with self.condition:
             if self.locked and self.lock_owner == request.client_id:
                 self.locked = False
                 self.lock_owner = None
                 self.condition.notify_all()
                 response = lock_pb2.Response(status=lock_pb2.Status.SUCCESS)
-                self.cache_response(request_id, response)
+                self.update_cache(request_id, response)
                 print(f"Lock released by client {request.client_id}")
                 return response
             elif not self.locked:
                 response = lock_pb2.Response(status=lock_pb2.Status.FILE_ERROR)
-                self.cache_response(request_id, response)
+                self.update_cache(request_id, response)
                 print(f"Client {request.client_id} attempted to release a lock that was not acquired.")
                 return response #Not a file error but all we have in proto?
             else:
                 response = lock_pb2.Response(status=lock_pb2.Status.FILE_ERROR)
-                self.cache_response(request_id, response)
+                self.update_cache(request_id, response)
                 print(f"Client {request.client_id} attempted to release a lock owned by client {self.lock_owner}.")
                 return response #Again Not file error
     
     def file_append(self, request, context):
-        print(f"Performing file append")
-        self.print_metadata(context)
-        # Get request ID and check if request is cached
-        request_id = self.get_request_id(context)
-        cached_response = self.return_cached_response(request_id, context)
-        if cached_response:
-            # Simmulating packet loss
-            print("Simulating packet loss: dropping response from server")
-            if random.random() < 0.3:
-                time.sleep(15)
-            return cached_response
-        else:
-            response = lock_pb2.Response(status=lock_pb2.Status.WORKING_ON_IT)
+        # Process request
+        response = self.process_request(context)
+        if response:
             return response
-        # If request is not cached, process request and cache response
+        # If there is no response ready, process the request and create a response
+        request_id = self.get_request_id(context)
         if self.lock_owner != request.client_id:
+            print(f"Simulating slow server")
+            time.sleep(7)
             response = lock_pb2.Response(status=lock_pb2.Status.FILE_ERROR)
-            self.cache_response(request_id, response)
+            self.update_cache(request_id, response)
             print(f"Client {request.client_id} does not have access to lock")
-            if random.random() < 0.7:
-                print("Simulating packet loss: dropping response from server")
-                time.sleep(15)
             return response
         if request.filename not in self.files:
+            print(f"Simulating slow server")
+            time.sleep(7)
             response = lock_pb2.Response(status=lock_pb2.Status.FILE_ERROR)
-            self.cache_response(request_id, response)
+            self.update_cache(request_id, response)
             print(f"Filename {request.filename} does not exist")
-            if random.random() < 0.7:
-                print("Simulating packet loss: dropping response from server")
-                time.sleep(15)
             return response
         print(f"Client {request.client_id} appended to file {request.filename}")
         with open(request.filename, 'ab') as file:
-            # Simulating slow server
-            print(f"Simulating slow server: sleeping for 20 seconds")
-            time.sleep(20)
+            print(f"Simulating slow server")
+            time.sleep(7)
             file.write(request.content)
         response = lock_pb2.Response(status=lock_pb2.Status.SUCCESS)
-        self.cache_response(request_id, response)
-        if random.random() < 0.7:
-            print("Simulating packet loss: dropping response from server")
-            time.sleep(15)
+        self.update_cache(request_id, response)
         return response
     
 if __name__ == "__main__":
